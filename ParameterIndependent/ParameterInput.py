c497be6ccf881727c1bd38d8d2b84b4cf2bb3f8b
hw454 <hw454@bath.ac.uk>
2019-04-16 15:14:23 +0100

    Adding directions to Parameter Input

--- ParameterIndependent/ParameterInput.py
+++ ParameterIndependent/ParameterInput.py
@@ -47,6 +47,37 @@ OuterBoundary=5*np.array([OuterBoundary1,OuterBoundary2, OuterBoundary3,
 Tx=np.array([5.75,5.25,1.5]) # -Router location -co-ordinate of three real numbers
 #(the third is zero when modelling in 2D).
 
+raylist       =np.empty([Nra+1, Nre+1,4])
+deltheta      =(-2+np.sqrt(2.0*(Nra)))*(ma.pi/(Nra-2))
+    xysteps       =int(2.0*ma.pi/deltheta)
+    zsteps        =int(ma.pi/deltheta-2)
+    Nra           =xysteps*zsteps+2
+    # ^^ Due to need of integer steps the input number of rays can not
+    # always be used if everything is equally spaced ^^
+    theta1        =deltheta*np.arange(xysteps)
+    theta2        =deltheta*np.arange(1,zsteps+1)
+    xydirecs      =np.transpose(r*np.vstack((np.cos(theta1),np.sin(theta1))))
+    z             =r*np.tensordot(np.cos(theta2),np.ones(xysteps),axes=0)
+    directions    =np.zeros((Nra,4))
+    directions[0] =np.array([0.0,0.0,r,0.0])
+    directions[-1]=np.array([0.0,0.0,-r,0.0])
+    # Form the xyz co-ordinates matrix
+    #FIXME try to form this without a loop
+    for j in range(1,zsteps+1):
+      st=(j-1)*xysteps+1
+      ed=(j)*xysteps+1
+      sinalpha=np.sin(theta2[j-1])
+      coords=np.c_[sinalpha*xydirecs,z[j-1]]
+      directions[st:ed]=np.c_[coords,np.zeros(xysteps)]
+    # Iterate through the rays find the ray reflections
+    # FIXME the rays are independent of each toher so this is easily parallelisable
+    for it in range(0,Nra):
+      Dir       =directions[it]
+      start     =np.append(Tx,[0])
+      raystart  =ry.Ray(start, Dir)
+      raystart.multiref(s,Nre)
+      raylist[it]=raystart.points[0:-2]
+
 np.save('Parameters/Obstacles.npy',Oblist)
 np.save('Parameters/OuterBoundary.npy',OuterBoundary)
 np.save('Parameters/Origin.npy',Tx)
