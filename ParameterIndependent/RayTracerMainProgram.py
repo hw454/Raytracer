42822fcad9ee7d001693d8a1b1a4bb7085ec4159
hw454 <hw454@bath.ac.uk>
2019-08-14 16:34:55 +0100

    No longer need freespace parameters for ref_coef calculation. Commented the functions for sphinx documentation.

--- ParameterIndependent/RayTracerMainProgram.py
+++ ParameterIndependent/RayTracerMainProgram.py
@@ -52,6 +52,66 @@ def RayTracer():
   return 1
 
 def MeshProgram():
+  ''' Refect rays and output the Mesh containing ray information.
+
+  Parameters for the raytracer are input in :py:mod:`ParameterInput`
+  The raytracing parameters defined in this module are saved and then loaded.
+
+  * 'Raytracing.npy' - An array of 3 floats which is saved to \
+  [Nra (number of rays), Nre (number of reflections), h (relative meshwidth)]
+  * 'Obstacles.npy'  - An array for 3x3x1 arrays containing co-ordinates \
+  forming triangles which form the obstacles. This is saved to Oblist \
+  (The obstacles which are within the outerboundary )
+  * 'Origin.npy'     - A 3x1 array for the co-ordinate of the source. \
+  This is saved to Tx  (The location of the source antenna and origin \
+  of every ray)
+  * 'OuterBoundary.npy' - An array for 3x3x1 arrays containing \
+  co-ordinates forming triangles which form the obstacles. This is \
+  saved to OuterBoundary   (The Obstacles forming the outer boundary of \
+  the room )
+  Put the two arrays of obstacles into one array
+
+  .. code::
+
+     Oblist=[Oblist,OuterBoundary]
+
+  * 'Directions.npy' - An Nrax3x1 array containing the vectors which \
+  correspond to the initial direction of each ray. This is save to Direc.
+
+  A room is initialised with *Oblist* using the :class:`room` \
+  class in :py:mod:`Room`.
+
+  The number of obstacles and the number of x, y and z steps is found
+
+  .. code::
+
+      Nob=Room.Nob
+      Nx=int(Room.maxxleng()/h)
+      Ny=int(Room.maxyleng()/h)
+      Nz=int(Room.maxzleng()/h)
+
+  Initialise a :py:mod:`DictionarySparseMatrix`. :py:class:`DS` with the \
+  number of spaces in the x, y and z axis Nx, Ny, Nz, the number of \
+  obstacles Nob, the number of reflections Nre and the number of rays Nra.
+
+  .. code::
+
+    Mesh=DSM.DS(Nx,Ny,Nz,int(Nob*Nre+1),int((Nre)*(Nra)+1))
+
+  Find the reflection points of the rays and store the distance and \
+  reflection angles of the rays in the Mesh. Use the :py:class:`room`. \
+  :func:`ray_mesh_bounce(Tx,Nre,Nra,Direc,Mesh)` function.
+
+  .. code::
+
+     Rays, Mesh=Room.ray_mesh_bounce(Tx,int(Nre),int(Nra),Direc,Mesh)
+
+  Save the reflection points in Rays to \
+  'RayMeshPoints\ **Nra**\ Refs\ **Nre**\ n.npy' making the \
+  substitution for **Nra** and **Nre** with their parameter values.
+
+  :return: Mesh
+  '''
   print('-------------------------------')
   print('Building Mesh')
   print('-------------------------------')
@@ -97,15 +157,31 @@ def MeshProgram():
   return Mesh
 
 def RefCoefComputation(Mesh):
+  ''' Compute the mesh of reflection coefficients.
+
+  :param Mesh: The DS mesh which contains the angles and distances rays \
+  have travelled.
+
+  Load the physical parameters using :py:mod:`ParameterInput`.\
+  :func:`.ObstacleCoefficients()`
+  * Znobrat - is the vector of characteristic impedances for obstacles \
+  divided by the characteristic impedance of air.
+  * refindex - if the vector of refractive indexes for the obstacles.
+
+  Compute the Reflection coefficients (RefCoefper,Refcoefpar) using:
+  :py:mod:`DictionarySparseMatrix`.\
+  :func:`ref_coef((Mesh,Znobrat,refindex)`
+  :return: (RefCoefper,Refcoefpar)
+  '''
   out=PI.ObstacleCoefficients()
-  FreeSpace=np.load('Parameters/FreeSpace.npy')
-  freq         =np.load('Parameters/frequency.npy')
-  Znob         =np.load('Parameters/Znob.npy')
+  #FreeSpace=np.load('Parameters/FreeSpace.npy')
+  #freq         =np.load('Parameters/frequency.npy')
+  Znobrat      =np.load('Parameters/Znobrat.npy')
   refindex     =np.load('Parameters/refindex.npy')
   print('-------------------------------')
   print('Computing the reflection coeficients')
   print('-------------------------------')
-  RefCoefPerp, RefCoefPar=DSM.ref_coef(Mesh,FreeSpace,freq,Znob,refindex)
+  RefCoefPerp, RefCoefPar=DSM.ref_coef(Mesh,Znobrat,refindex)
   print('-------------------------------')
   print('Reflection coeficients found')
   print('-------------------------------')
