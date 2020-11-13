1c1103452b9561f128b979fb2d5f8a3d98ab76f3
hw454 <hw454@bath.ac.uk>
2020-11-13 14:56:41 +0000

    Bash script for trying transmitter locations

--- ParameterIndependent/Raytracer_transmittermover.sh
+++ ParameterIndependent/Raytracer_transmittermover.sh
@@ -2,6 +2,7 @@
 # Keith Briggs 2020-11-02 & Hayley Wragg
 # bash Raytracer_transmittermover.sh
 
+./ParameterLoad.py
 for ((job=0; job<250; job++)); do
   echo "job=${job}"
   time python3 RayTracerMainProgram.py "${job}"
