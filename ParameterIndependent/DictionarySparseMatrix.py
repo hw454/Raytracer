Fixed the gain optimal finder to use the pseudoinverse and output the gains not the square root of the gains. Corrected the field calculation also to include this. The optimisation method uses the transmitter location not job number to save results since multiple positions for Tx are considered in the same voxel. RaytracerPlotiting fixed loading of quality files.

ParameterIndependent/DictionarySparseMatrix.py
@@ -1645,12 +1645,11 @@ class DS:
       Amat=np.matmul(F.transpose(),F)+np.matmul(H.transpose(),H)
       for i,j in product(range(Nra),range(Nra)):
         Atot[i,j]+=Amat[i,j].copy()
-    Ainv=inv(Atot)
+    Ainv=pinv(Atot)
     Aout=np.zeros((Nra,1),float)
     for i,j in product(range(Nra),range(Nra)):
-      Aout[i,0]+=Ainv[i,j]**2
-    print(Aout,np.sqrt(sum(Aout)))
-    Aout/=np.sqrt(sum(Aout))
+      Aout[i,0]+=Ainv[i,j]
+    Aout/=leng(Aout)
     return Aout
   def gain_phase_rad_ref_mul_add(s,Com1,Com2,G,khat,L,lam,Nra=0,ind=-1):
     """ Multiply all terms of s elementwise with Com1/Rad and each row by Gt.
@@ -1695,8 +1694,8 @@ class DS:
           nra=b%Nra
         else:
           nra=b
-        out1[x,y,z]+=G[nra-1,0]*k*Com1[x,y,z][a,b]
-        out2[x,y,z]+=G[nra-1,0]*k*Com2[x,y,z][a,b]
+        out1[x,y,z]+=np.sqrt(G[nra-1,0])*k*Com1[x,y,z][a,b]
+        out2[x,y,z]+=np.sqrt(G[nra-1,0])*k*Com2[x,y,z][a,b]
       if x==xcheck and y==ycheck and z==zcheck and dbg:
         logging.info('This is an information message')
         logmessage='At position (%d,%d,%d)'%(x,y,z)
@@ -2389,6 +2388,7 @@ def optimum_gains(foldtype,Mesh,room,Znobrat,refindex,Antpar, Pol,Nra,Nre,Ns,LOS
   t4=t.time()
   Hx,Fx=RadMesh.opti_func_mats(Realper,Realpar,Imageper,Imagepar,khat,L,lam,Pol,Nra,ind)
   Gt=Hx.opti_combo_inverse(Fx,Nra)
+  Gt=np.power(Gt,2)
   return Gt
 def power_compute(foldtype,Mesh,room,Znobrat,refindex,Antpar,Gt, Pol,Nra,Nre,Ns,LOS=0,PerfRef=0,ind=-1):
   ''' Compute the field from a Mesh of ray information and the physical \

ParameterIndependent/OptimisationMethod.py
@@ -144,7 +144,7 @@ def Quality_Tx(Tx):
   Nz=int(Room.maxzleng()/h)
   Ns=max(Nx,Ny,Nz)
   meshfolder='./Mesh/'+plottype+'/Nra%03dRefs%03dNs%0d'%(Nr,Nre,Ns)
-  meshname=meshfolder+'/DSM_tx%03d'%(job)
+  meshname=meshfolder+'/DSM_tx%03dx%03dy%03dz'%(Tx[0],Tx[1],Tx[2])
 
   if os.path.isfile(meshname):
     Mesh= DSM.load_dict(meshname,Nx,Ny,Nz)
@@ -192,7 +192,7 @@ def Quality_Tx(Tx):
   Q,_=DSM.quality_compute(plottype,Mesh,Grid,Room,Znobrat,refindex,Antpar,Gt, Pol,Nr,Nre,Ns,LOS,PerfRef)
   return -Q
 
-def Quality_MoreInputs(Tx,Direc,programterms,RayPar,foldtype,Room,Znobrat,refindex,Antpar,Gt, Pol,LOS,PerfRef):
+def Quality_MoreInputs(Tx,Direc,programterms,RayPar,foldtype,Room,Znobrat,refindex,Antpar,Gt, Pol,LOS,PerfRef,Boxstr,Obstr,index=0):
   ''' Reflect rays and output the Mesh containing ray information. \
   This mesh contains the distance rays have travelled and the angles of reflection.
 
@@ -273,7 +273,6 @@ def Quality_MoreInputs(Tx,Direc,programterms,RayPar,foldtype,Room,Znobrat,refind
   Nsur                        =Room.Nsur
   if not 0<=Tx[0]<Room.maxxleng() and not 0<=Tx[1]<Room.maxyleng() and not 0<=Tx[2]<Room.maxzleng():
     return -1000
-  job=RT.jobfromTx(Tx,h)
   if abs(Tx[0]-0.5)<epsilon and abs(Tx[1]-0.5)<epsilon and abs(Tx[2]-0.5)<epsilon:
     loca='Centre'
   else:
@@ -285,7 +284,7 @@ def Quality_MoreInputs(Tx,Direc,programterms,RayPar,foldtype,Room,Znobrat,refind
   Nz=int(Room.maxzleng()/h)
   Ns=max(Nx,Ny,Nz)
   meshfolder='./Mesh/'+foldtype+'/Nra%03dRefs%03dNs%0d'%(Nr,Nre,Ns)
-  meshname=meshfolder+'/DSM_tx%03d'%(job)
+  meshname=meshfolder+'/DSM_tx%03dx%03dy%03dz'%(Tx[0],Tx[1],Tx[2])
   if os.path.isfile(meshname):
     Mesh= DSM.load_dict(meshname,Nx,Ny,Nz)
   else:
@@ -298,11 +297,17 @@ def Quality_MoreInputs(Tx,Direc,programterms,RayPar,foldtype,Room,Znobrat,refind
       return 0
     #-----------The input directions changes for each ray number.-------
     Rays, Mesh=Room.ray_mesh_bounce(Tx,Direc,Mesh,programterms)
+    Mesh.save_dict(meshname)
   # Initialise Grid For Power-------------------------------------
   Ns=max(Nx,Ny,Nz)
   Grid=np.zeros((Nx,Ny,Nz),dtype=float)
   #Gt=DSM.optimum_gains(plottype,Mesh,Room,Znobrat,refindex,Antpar, Pol,Nra,Nre,Ns,LOS,PerfRef)
   Q,_=DSM.quality_compute(foldtype,Mesh,Grid,Room,Znobrat,refindex,Antpar,Gt, Pol,Nr,Nre,Ns,LOS,PerfRef)
+  if not os.path.exists('./Quality'):
+   os.makedirs('./Quality')
+  if not os.path.exists('./Quality/'+plottype):
+    os.makedirs('./Quality/'+plottype)
+  np.save('./Quality/'+plottype+'/'+Boxstr+Obstr+'Quality%03dRefs%03dm%03d_tx%03dx%03dy%03dz.npy'%(Nr,Nre,index,Tx[0],Tx[1],Tx[2]),Q)
   return -Q
 
 def MoreInputs_Run(index=0):
@@ -410,19 +415,18 @@ def MoreInputs_Run(index=0):
   Antpar        =np.load('Parameters/Antpar%03d.npy'%index)
   Tx0=np.array([0.4,0.4,0.4])
   #Q=Quality_MoreInputs(Tx0,Direc,programterms,RayPar,foldtype,Room,Znobrat,refindex,Antpar,Gt, Pol,LOS,PerfRef)
-  pars=(Direc,programterms,RayPar,foldtype,Room,Znobrat,refindex,Antpar,Gt, Pol,LOS,PerfRef)
-
-  TxB=((0,h*Nx),(0,h*Ny),(0,h*Nz))
+  pars=(Direc,programterms,RayPar,foldtype,Room,Znobrat,refindex,Antpar,Gt, Pol,LOS,PerfRef,Box,Obstr)
+  TxB=np.array([(0,h*Nx),(0,h*Ny),(0,h*Nz)])
   Tx=Tx0
-  TxOut=minimize(Quality_MoreInputs, Tx, method='Powell',args=pars, tol=1e-6,bounds=TxB)
-
+  TxOut=minimize(Quality_MoreInputs, Tx, method='Powell',args=pars, tol=1e-1,bounds=TxB)
+  print('Optimal Tx at ',TxOut.x)
   ResultsFolder='./OptimisationResults'
-  SpecResulstsFolder=ResultsFolder+'/'+plottype+'/Nra%03dRefs%03dNs%0d'%(Nr,Nre,Ns)
+  SpecResultsFolder=ResultsFolder+'/'+plottype+'/Nra%03dRefs%03dNs%0d'%(Nr,Nre,Ns)
   if not os.path.exists(ResultsFolder):
     os.makedirs(ResultsFolder)
     os.makedirs(ResultsFolder+'/'+plottype)
     os.makedirs(SpecResultsFolder)
-  if not os.path.exists(Resultsfolder+'/'+plottype):
+  if not os.path.exists(ResultsFolder+'/'+plottype):
     os.makedirs(ResultsFolder+'/'+plottype)
     os.makedirs(SpecResultsFolder)
   if not os.path.exists(SpecResultsFolder):
@@ -430,25 +434,25 @@ def MoreInputs_Run(index=0):
   np.save(SpecResultsFolder+'/OptimumOrigin.npy',TxOut.x)
   print('moreinputs',TxOut)
   meshfolder='./Mesh/'+plottype+'/Nra%03dRefs%03dNs%0d'%(Nr,Nre,Ns)
-  job=RT.jobfromTx(Txout.x,h)
-  meshname=meshfolder+'/DSM_tx%03d'%(job)
+  job=RT.jobfromTx(TxOut.x,h)
+  meshname=meshfolder+'/DSM_tx%03dx%03dy%03dz'%(TxOut.x[0],TxOut.x[1],TxOut.x[2])
   if os.path.isfile(meshname):
     Mesh= DSM.load_dict(meshname,Nx,Ny,Nz)
   else:
     print('The optimal Tx does not have a corresponding Mesh')
   P,_=power_compute(plottype,Mesh,Room,Znobrat,refindex,Antpar,Gt, Pol,Nr,Nre,Ns,LOS,PerfRef)
-  np.save(meshfolder+'/'+Box+Obstr+'Power_grid%03dRefs%03dm%03d_tx%03d.npy'%(Nr,Nre,index,job),P)
+  np.save(meshfolder+'/'+Box+Obstr+'Power_grid%03dRefs%03dm%03d_tx%03dx%03dy%03dz.npy'%(Nr,Nre,index,TxOut.x[0],TxOut.x[1],TxOut.x[2]),P)
   RadAstr=meshfolder+'/RadA_grid%dRefs%dm%d.npy'%(Nr,Nre,index)
   if os.path.isfile(RadAstr):
-    os.rename(r''+meshfolder+'/RadA_grid%dRefs%dm%d.npy'%(Nr,Nre,index),r''+meshfolder+'/'+Box+'RadA_grid%dRefs%dm%d_tx%03d.npy'%(Nr,Nre,index,job))
+    os.rename(r''+meshfolder+'/RadA_grid%dRefs%dm%d.npy'%(Nr,Nre,index),r''+meshfolder+'/'+Box+'RadA_grid%dRefs%dm%d_tx%03dx%03dy%03dz.npy'%(Nr,Nre,index,TxOut.x[0],TxOut.x[1],TxOut.x[2]))
   if not LOS:
     Angstr='./Mesh/'+plottype+'/AngNpy.npy'
     if os.path.isfile(Angstr):
-      os.rename(r''+meshfolder+'/AngNpy.npy',r''+meshfolder+'/'+Box+'AngNpy%03dRefs%03dNs%03d_tx%03d.npy'%(Nr,Nre,Ns,job))
+      os.rename(r''+meshfolder+'/AngNpy.npy',r''+meshfolder+'/'+Box+'AngNpy%03dRefs%03dNs%03d_tx%03dx%03dy%03dz.npy'%(Nr,Nre,Ns,TxOut.x[0],TxOut.x[1],TxOut.x[2]))
     for su in range(0,Nsur):
       RadSstr=meshfolder+'/RadS%d_grid%dRefs%dm%d.npy'%(su,Nr,Nre,index)
       if os.path.isfile(RadSstr):
-        os.rename(r''+meshfolder+'/RadS%d_grid%dRefs%dm%d.npy'%(su,Nr,Nre,index),r''+meshfolder+'/'+Box+'RadS%d_grid%dRefs%dm%d_tx%03d.npy'%(su,Nr,Nre,0,job))
+        os.rename(r''+meshfolder+'/RadS%d_grid%dRefs%dm%d.npy'%(su,Nr,Nre,index),r''+meshfolder+'/'+Box+'RadS%d_grid%dRefs%dm%d_tx%03dx%03dy%03dz.npy'%(su,Nr,Nre,index,TxOut.x[0],TxOut.x[1],TxOut.x[2]))
   myfile = open('Parameters/PlotFit.txt', 'rt') # open lorem.txt for reading text
   plotfit= myfile.read()         # read the entire file into a string
   myfile.close()

ParameterIndependent/RayTracerPlottingFunctions.py
@@ -426,6 +426,10 @@ def plot_quality_contour(plottype,testnum,roomnumstat):
   plotfit= myfile.read()         # read the entire file into a string
   myfile.close()
 
+  if Nre>1:
+    Refstr=nw.num2words(Nre)+'Ref'
+  else:
+    Refstr='NoRef'
   if InnerOb:
     boxstr='Box'
   else:
@@ -448,8 +452,7 @@ def plot_quality_contour(plottype,testnum,roomnumstat):
         LOSstr='MultiRef'
     else:
       LOSstr='SingleRef'
-
-  foldtype=LOSstr+boxstr
+  foldtype=Refstr+boxstr
   #Room contains all the obstacles and walls.
   Room=rom.room(Oblist,Ntri)
   Nob=Room.Nob
@@ -711,7 +714,7 @@ def main():
   testnum    =SimPar.cell(row=17,column=3).value
   roomnumstat=SimPar.cell(row=18,column=3).value
   #plot_times(plottype,testnum,roomnumstat)
-  plot_grid(plottype,testnum,roomnumstat)        # Plot the power in slices.
+  #plot_grid(plottype,testnum,roomnumstat)        # Plot the power in slices.
   plot_quality_contour(plottype,testnum,roomnumstat)
   ResOn      =np.load('Parameters/ResOn.npy')
   if ResOn: