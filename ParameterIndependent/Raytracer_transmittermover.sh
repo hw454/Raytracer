d548279307ff678ec6c5068a78e3461cef8e3dc4
hw454 <hw454@bath.ac.uk>
2021-03-16 19:55:22 +0000

    Push and pull files within script

--- ParameterIndependent/Raytracer_transmittermover.sh
+++ ParameterIndependent/Raytracer_transmittermover.sh
@@ -3,7 +3,25 @@
 # bash Raytracer_transmittermover.sh
 
 python ParameterLoad.py
-for ((job=0; job<126; job++)); do
+for ((job=0; job<125; job++)); do
   echo "job=${job}"
+  git pull git@github.com:/hw454/Raytracer.git
   time python RayTracerMainProgram.py "${job}"
+  git pull git@github.com:/hw454/Raytracer.git
+  git add Mesh Quality OptimisationResults
+  git commit -m "Mesh files to share with server"
+  git push git@github.com:/hw454/Raytracer.git
 done
+git pull git@github.com:/hw454/Raytracer.git
+time python RayTracerPlottingFunctions.py
+git pull git@github.com:/hw454/Raytracer.git
+git add Quality OptimisationResults
+git commit -m "Mesh files to share with server"
+git push git@github.com:/hw454/Raytracer.git
+git pull git@github.com:/hw454/Raytracer.git
+time python OptimisationMethod.py
+git pull git@github.com:/hw454/Raytracer.git
+git add Quality OptimisationResults
+git commit -m "Mesh files to share with server"
+git push git@github.com:/hw454/Raytracer.git
+
